b395adffc2f9f12f9c87ad780e51e8a4bb171d26
gibwaff <igorbel1806@gmail.com>
2025-03-24 22:42:39 +0300

    выполнил

---
+++ Warcode/6 kyu/Find The Parity Outlier/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func FindOutlier(integers []int) (res int) {
+	m0 := integers[0] % 2
+	m1 := integers[1] % 2
+	if m0 == m1 {
+		if m0 == 0 {
+			for _, el := range integers {
+				if el%2 != 0 {
+					res = el
+					break
+				}
+			}
+		} else {
+			for _, el := range integers {
+				if el%2 == 0 {
+					res = el
+					break
+				}
+			}
+		}
+	} else {
+		m2 := integers[2] % 2
+		if m2 == m0 {
+			res = integers[1]
+		} else {
+			res = integers[0]
+		}
+	}
+
+	return
+}
+
+func main() {
+	fmt.Println(FindOutlier([]int{2, 6, 8, -10, 3})) // Output: 3
+}
