9c2984dd53e9a422d38cd4be0dbfc419272b0ed8
gibwaff <igorbel1806@mail.ru>
2025-03-26 22:53:41 +0300

    solv

---
+++ Warcode/6 kyu/Equal Sides Of An Array/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func FindEvenIndexx(nums []int) int {
+	cur, left_s, right_s := 0, 0, 0
+	for i := 1; i < len(nums); i++ {
+		right_s += nums[i]
+	}
+	for ; cur < len(nums)-1; cur++ {
+		if left_s == right_s {
+			return cur
+		}
+		left_s += nums[cur]
+		right_s -= nums[cur+1]
+	}
+	if left_s == right_s {
+		return cur
+	}
+	return -1
+
+}
+
+func main() {
+	arr := []int{1, 100, 50, -51, 1, 1}
+	fmt.Println(FindEvenIndexx(arr))
+}
