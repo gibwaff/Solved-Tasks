642b523ed7bc7d7eb94468e12872e34b5b4d85ce
gibwaff <igorbel1806@mail.ru>
2025-03-28 00:10:51 +0300

    done 28.03

--- Warcode/5 kyu/Moving Zeros To The End/main.go
+++ Warcode/5 kyu/Moving Zeros To The End/main.go
@@ -26,6 +26,6 @@ func MoveZeros(arr []int) []int {
 }
 
 func main() {
-	arr := []int{1, 2, 0, 1, 0, 1, 0, 3, 0, 1}
-	fmt.Println(MoveZeros(arr))
+	arr := []int{0, 1, 0, 3, 12}
+	fmt.Println(MoveZeros(arr)) // [1 3 12 0 0]
 }
